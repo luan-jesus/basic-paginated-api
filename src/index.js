44f9a54c8c0455fcf443cbe34f121232a9f3f24d
luan.teixeira <luan.teixeira@izatta.tech>
2025-08-16 11:19:49 -0300

    change host

--- src/index.js
+++ src/index.js
@@ -110,6 +110,6 @@ function formatResponse(rows) {
 }
 
 const PORT = process.env.API_PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
