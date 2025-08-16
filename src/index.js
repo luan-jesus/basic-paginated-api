b0da92d6c767bfb0c6d3587342cf0be551301799
luan.teixeira <luan.teixeira@izatta.tech>
2025-08-16 11:09:35 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -12,6 +13,7 @@ const pool = new Pool({
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/operacoes', async (req, res) => {
   const { codigoClassificador, dataContabil } = req.query;
